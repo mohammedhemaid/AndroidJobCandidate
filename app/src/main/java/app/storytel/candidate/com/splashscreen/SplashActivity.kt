fc5b12561ebfa570ce40696a9d6ba6f0fc38c0c9
mohammedhemaid <m7md972013@gmail.com>
2020-06-11 10:34:14 +0300

    Add Splash Screen

---
+++ app/src/main/java/app/storytel/candidate/com/splashscreen/SplashActivity.kt
@@ -0,0 +1,22 @@
+package app.storytel.candidate.com.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.storytel.candidate.com.postList.PostListActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (!isTaskRoot
+                && intent.hasCategory(Intent.CATEGORY_LAUNCHER)
+                && intent.action != null && intent.action == Intent.ACTION_MAIN) {
+            finish()
+            return
+        }
+
+        startActivity(Intent(this, PostListActivity::class.java))
+        finish()
+    }
+}
